eb381cdcb065f7da4be5d79731f7cabbfebd5b2a
kyleyiyang <kyleyiyang@gmail.com>
2021-05-20 21:52:20 -0400

    Update mergeKLists.java

--- mergeKLists.java
+++ mergeKLists.java
@@ -37,3 +37,48 @@ class Solution {
         return dumy.next;
     }
 }
+
+*******************************************************
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length==0) return null;
+        if (lists.length==1) return lists[0];
+        int size=1;
+        while (size<lists.length) {
+            for (int i=0;i+size<lists.length;i=i+size*2) {
+                lists[i]=merge(lists[i],lists[i+size]);
+            }
+            size*=2;
+        }
+        return lists[0];
+    }
+    public ListNode merge(ListNode list1,ListNode list2) {
+        ListNode dumy=new ListNode();
+        ListNode temp=dumy;
+        while (list1!=null && list2!=null) {
+            if (list1.val<list2.val) {
+                temp.next=list1;
+                list1=list1.next;
+                temp=temp.next;
+                
+            } else {
+                temp.next=list2;
+                list2=list2.next;
+                temp=temp.next;
+            }
+        }
+        temp.next=(list1==null) ? list2:list1;
+        return dumy.next;
+    }
+}
